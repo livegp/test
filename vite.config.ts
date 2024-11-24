d98f93df50839c7980ee154d677ee0ee691cff63
Pishta Oleksandr <live.gp@gmail.com>
2024-11-24 13:11:18 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/test",
+  base: "/test/",
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
